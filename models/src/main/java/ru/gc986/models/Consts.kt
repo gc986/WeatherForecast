ea0447fccf930f5437545e44aebaecb20adee2cb
igor_yazovcev <uSAg9wbxUX6NEROI>
2020-04-24 13:50:02 +0500

    Fixed time

--- models/src/main/java/ru/gc986/models/Consts.kt
+++ models/src/main/java/ru/gc986/models/Consts.kt
@@ -5,7 +5,7 @@ class Consts {
     companion object {
         const val DEFAULT_INT_SP = 0
 
-        const val ONE_SEC_IN_MILLISECONDS = 60000L
+        const val ONE_SEC_IN_MILLISECONDS = 1000L
         const val ONE_MIN_IN_SECONDS = 60L
         const val TEN_MINUTES = 10L * ONE_MIN_IN_SECONDS * ONE_SEC_IN_MILLISECONDS
 
